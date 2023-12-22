b0fc22c038bab4733bf793b1336bf677cbeb7259
Dominichigozie2 <chigoziedomnic@gmail.com>
2023-12-22 13:45:01 +0100

    made my first changes

--- scripts/script.js
+++ scripts/script.js
@@ -29,3 +29,35 @@ searchClicked.addEventListener('click', () =>{
     });
 });
 
+
+const winbtnlist = document.querySelector(".winners");
+const winbody = document.querySelector(".col-3");
+const winbodyClosed = document.querySelector(".close");
+const itemOne = document.querySelector(".item-1");
+
+
+winbtnlist.addEventListener('click', ()=>{
+    winbody.style.display="flex";
+    winbodyClosed.addEventListener('click', ()=>{
+        const itemTwo = document.querySelector(".item-3");
+        winbody.style.display="none";
+        itemOne.classList.add("active");
+        itemTwo.classList.remove("active");
+    });
+});
+
+
+
+const listItems = document.querySelectorAll('.bottom-list-items');
+listItems.forEach(item => {
+    item.addEventListener('click', handleClick);
+});
+
+function handleClick(event) {
+    const previousActiveItem = document.querySelector('.bottom-list-items.active');
+    if (previousActiveItem) {
+        previousActiveItem.classList.remove('active');
+    }
+
+    event.currentTarget.classList.add('active'); // Use event.currentTarget
+}
